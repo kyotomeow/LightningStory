25ee2e112897b80355185c7c43b9092377f76dd4
kyotomeow <dawid222777@mail.ru>
2026-01-05 05:49:19 +0700

    Test

--- pong/main.cpp
+++ pong/main.cpp
@@ -9,6 +9,12 @@
 
 using namespace std;
 
+struct {
+    HWND hWnd;//хэндл окна
+    HDC device_context, context;// два контекста устройства (для буферизации)
+    int width, height;//сюда сохраним размеры окна которое создаст программа
+} window;
+
 struct sprite {
     float x, y, width, height;
     HBITMAP hBitmap;
@@ -47,87 +53,24 @@ struct Enemy : Character {
     float StartX, StartY;
 };
 
-
-
-
-
-
-
-// секция данных игры  
-//typedef struct {
-//    float x, y, width, height, rad, dx, dy, speed;
-//    int hp, atackPower, current_loc, maxHp,num;
-//    string name;
-//    HBITMAP hBitmap;//хэндл к спрайту
-//} sprite;
-//
-//
-//sprite hero;//герой
-//sprite enemy[2];
-//sprite platform[5];
-//
-//struct {
-//    HWND hWnd;//хэндл окна
-//    HDC device_context, context;// два контекста устройства (для буферизации)
-//    int width, height;//сюда сохраним размеры окна которое создаст программа
-//} window;
-
-int gravity = 18;
-int all;
-bool jump = false;
-
 HBITMAP hBack;// хэндл для фонового изображения
 
+    vector<Platforms> platform;
 
 //cекция кода
 
 void InitGame()
 {
-    hero.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    enemy[0].hBitmap = (HBITMAP)LoadImageA(NULL, "racket_enemy.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    for (int i = 0; i <= 4; i++) {
+    //hero.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    //enemy[0].hBitmap = (HBITMAP)LoadImageA(NULL, "racket_enemy.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    for (int i = 0; i <= 1; i++) {
         platform[i].hBitmap = (HBITMAP)LoadImageA(NULL, "racket_enemy.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     }
     hBack = (HBITMAP)LoadImageA(NULL, "back.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     //------------------------------------------------------
+    platform.push_back(Platforms(200, window.height - 50, 350, 30));
+    platform.push_back(Platforms(window.width, 500, 300, 30));
 
-    hero.x = 0;
-    hero.height = 180;
-    hero.y = window.height - hero.height;
-    hero.width = 90;
-
-    hero.speed = 15;
-    //------------------------------------------------------
-    platform[0].x = 150;
-    platform[0].width = 350;
-    platform[0].y = 700;
-    platform[0].height = 30;
-
-    platform[1].x = window.width / 2 - 350;
-    platform[1].width = 700;
-    platform[1].y = 450;
-    platform[1].height = 30;
-
-    platform[2].width = 350;
-    platform[2].x = window.width - platform[2].width;
-    platform[2].y = window.height - 200;
-    platform[2].height = 30;
-
-    platform[3].width = 350;
-    platform[3].x = window.width - platform[3].width;
-    platform[3].y = window.height / 2 - 200;
-    platform[3].height = 30;
-
-    platform[4].x = 0;
-    platform[4].width = 350;
-    platform[4].y = 200;
-    platform[4].height = 30;
-    //------------------------------------------------------
-    enemy[0].x = platform[3].x;
-    enemy[0].height = 180;
-    enemy[0].y = platform[2].y - enemy[0].height;
-    enemy[0].width = 90;
-    enemy[0].speed = 12;
 }
 
 void ProcessSound(const char* name)
@@ -136,22 +79,22 @@ void ProcessSound(const char* name)
         //ProcessSound("bounce.wav");
 }
 
-void ProcessInput()
-{
-    if (GetAsyncKeyState(VK_LEFT)) hero.x -= hero.speed;
-    if (GetAsyncKeyState(VK_RIGHT)) hero.x += hero.speed;
-}
+//void ProcessInput()
+//{
+//    if (GetAsyncKeyState(VK_LEFT)) hero.x -= hero.speed;
+//    if (GetAsyncKeyState(VK_RIGHT)) hero.x += hero.speed;
+//}
 
 
-void GravityAndJump() {
-
-    hero.y += gravity;
-
-    if (GetAsyncKeyState(VK_SPACE))
-    {
-        hero.y -= 110;
-    }
-}
+//void GravityAndJump() {
+//
+//    hero.y += gravity;
+//
+//    if (GetAsyncKeyState(VK_SPACE))
+//    {
+//        hero.y -= 110;
+//    }
+//}
 
 void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool alpha = false)
 {
@@ -184,27 +127,23 @@ void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool
 void ShowSprites()
 {
     ShowBitmap(window.context, 0, 0, window.width, window.height, hBack);//задний фон
-    ShowBitmap(window.context, hero.x, hero.y, hero.width, hero.height, hero.hBitmap);
-    ShowBitmap(window.context, enemy[0].x, enemy[0].y, enemy[0].width, enemy[0].height, enemy[0].hBitmap);
-    ShowBitmap(window.context, platform[0].x, platform[0].y, platform[0].width, platform[0].height, platform[0].hBitmap);
-    ShowBitmap(window.context, platform[1].x, platform[1].y, platform[1].width, platform[1].height, platform[1].hBitmap);
-    ShowBitmap(window.context, platform[2].x, platform[2].y, platform[2].width, platform[2].height, platform[2].hBitmap);
-    ShowBitmap(window.context, platform[3].x, platform[3].y, platform[3].width, platform[3].height, platform[3].hBitmap);
-    //ShowBitmap(window.context, platform[4].x, platform[4].y, platform[4].width, platform[4].height, platform[4].hBitmap);
+    ShowBitmap(window.context, platform[0].x, platform[0].y, platform[0].width, platform[0].height, hBack);
+    //ShowBitmap(window.context, hero.x, hero.y, hero.width, hero.height, hero.hBitmap);
+    
 }
 
 
-void WallsCheck()
-{
-    hero.x = max(hero.x, 0);//если коодината левого угла ракетки меньше нуля, присвоим ей ноль
-    hero.x = min(hero.x, window.width - hero.width);//аналогично для правого угла
-}
-
-void RoofAndFloorCheck()
-{
-    hero.y = max(hero.y, 0);
-    hero.y = min(hero.y,window.height - hero.height);
-}
+//void WallsCheck()
+//{
+//    hero.x = max(hero.x, 0);//если коодината левого угла ракетки меньше нуля, присвоим ей ноль
+//    hero.x = min(hero.x, window.width - hero.width);//аналогично для правого угла
+//}
+//
+//void RoofAndFloorCheck()
+//{
+//    hero.y = max(hero.y, 0);
+//    hero.y = min(hero.y,window.height - hero.height);
+//}
 
 
 bool CheckCollisions(float firstx, float firsty, float firstw, float firsth,
@@ -252,36 +191,33 @@ void WorkCollisions(float& firstx, float& firsty, float& firstw, float& firsth, 
         
     }
 }
-void ShowScore()
-{
-    //поиграем шрифтами и цветами
-    SetTextColor(window.context, RGB(160, 160, 160));
-    SetBkColor(window.context, RGB(0, 0, 0));
-    SetBkMode(window.context, TRANSPARENT);
-    auto hFont = CreateFont(70, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 2, 0, "CALIBRI");
-    auto hTmp = (HFONT)SelectObject(window.context, hFont);
-
-    char txt[32];//буфер для текста
-    _itoa_s(hero.x, txt, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
-    TextOutA(window.context, 10, 10, "Score", 5);
-    TextOutA(window.context, 200, 10, (LPCSTR)txt, strlen(txt));
-
-    _itoa_s(hero.y, txt, 10);
-    TextOutA(window.context, 10, 100, "Balls", 5);
-    TextOutA(window.context, 200, 100, (LPCSTR)txt, strlen(txt));
-}
+//void ShowScore()
+//{
+//    //поиграем шрифтами и цветами
+//    SetTextColor(window.context, RGB(160, 160, 160));
+//    SetBkColor(window.context, RGB(0, 0, 0));
+//    SetBkMode(window.context, TRANSPARENT);
+//    auto hFont = CreateFont(70, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 2, 0, "CALIBRI");
+//    auto hTmp = (HFONT)SelectObject(window.context, hFont);
+//
+//    char txt[32];//буфер для текста
+//    _itoa_s(hero.x, txt, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+//    TextOutA(window.context, 10, 10, "Score", 5);
+//    TextOutA(window.context, 200, 10, (LPCSTR)txt, strlen(txt));
+//
+//    _itoa_s(hero.y, txt, 10);
+//    TextOutA(window.context, 10, 100, "Balls", 5);
+//    TextOutA(window.context, 200, 100, (LPCSTR)txt, strlen(txt));
+//}
         //ProcessSound("bounce.wav"); platform[0].y >= hero.y
-void ProcessRoom()
-{
-    RoofAndFloorCheck();
-    for (int i = 0; i <= 4; i++) {
-        WorkCollisions(hero.x, hero.y, hero.width, hero.height, platform[i].x, platform[i].y, platform[i].width, platform[i].height);
-    }
-
-    /*CheckCollisions(hero.x, hero.y, hero.width, hero.height, platform[0].x, platform[0].y, platform[0].width, platform[0].height);
-    WorkCollisions(hero.x, hero.y, hero.width, hero.height, platform[0].x, platform[0].y, platform[0].width, platform[0].height);*/
-    
-}
+//void ProcessRoom()
+//{
+//    RoofAndFloorCheck();
+//    for (int i = 0; i <= 4; i++) {
+//        WorkCollisions(hero.x, hero.y, hero.width, hero.height, platform[i].x, platform[i].y, platform[i].width, platform[i].height);
+//    }
+//
+//}
 
 void InitWindow()
 {
@@ -313,13 +249,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
         ShowSprites();//рисуем фон, ракетку и шарик
-        ShowScore();
+        //ShowScore();
         BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
-        Sleep(16);//Bждем 16 милисекунд (1/количество кадров в секунду)
+        Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду)
         
-        ProcessRoom();//обрабатываем отскоки от стен и каретки, попадание шарика в картетку
-        ProcessInput();//опрос клавиатуры
-        WallsCheck();//проверяем, чтобы ракетка не убежала за экран
-        GravityAndJump();
+        //ProcessRoom();//обрабатываем отскоки от стен и каретки, попадание шарика в картетку
+        //ProcessInput();//опрос клавиатуры
+        //WallsCheck();//проверяем, чтобы ракетка не убежала за экран
+        //GravityAndJump();
     }
 }
